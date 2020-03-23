65fb3fa66ec74d53e2f4814efa0c4b7a42e56c3e
pbcarloz <pb_carlos@outlook.com>
2020-03-22 18:02:32 -0600

    saving work

--- Exercise.js
+++ Exercise.js
@@ -5,4 +5,53 @@ let tankLevel = false;
 if (experience > 90 ){
     let tankLevel = true;
 }
+const a = function (){
+    console.log("a");
+}
+
+const obj = {
+    player: "Carlos",
+    experience: 100,
+    tankLevel: false
+}
+
+obj.tankLevel = true;
+
+// DESTRUCTURING //////////////////////////////////////////////////////////////////
+
+const obj = {
+    player: "Carlos",
+    experience: 100,
+    tankLevel: false
+}
+
+const player = obj.player;
+const experience = obj.experience;
+let tankLevel = obj.tankLevel;
+
+// syntax para hacer lo mismo/////////////////
+
+const {player, experience} = obj;
+let {tankLevel} = obj;
+
+// OBJECT PROPERTIES/////////////////////
+
+const name = "Carlos Barcenas";
+
+const obj = {
+    [name]: "hello",
+    ["ray" + "smith"]: "hihi"
+}
+
+const a = "carlos";
+const b = true;
+const c = {};
+
+const obj = {
+    a,
+    b,
+    c
+}
+
+// Template Strings//////////////////////////////
 
