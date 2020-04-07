d64e0d2465f23fe0edf23bbc6c3070247d0f8897
pbcarloz <pb_carlos@outlook.com>
2020-04-07 15:21:50 -0500

    Adding New Files

---
+++ Advanced Looping.js
@@ -0,0 +1,114 @@
+const basket = ["manzanas","duraznos","uvas"];
+
+for ( let i = 0; i < basket.length; i++){
+    console.log(basket[i]);
+}
+
+basket.forEach(itenm => {
+    console.log(item);
+})
+// for of
+// iterating - arrays, strings
+for (item of basket){
+    console.log(item);
+}
+for (item of "basket"){
+    console.log(item);
+}
+// for in - properties
+// enumerating - objects
+
+const detailedBasket ={
+    apples: 5,
+    oranges: 10,
+    grapes: 20
+}
+
+for (item in detailedBasket){
+    console.log(item);
+}
+
+const basket = ['apples', 'oranges', 'grapes'];
+const detailedBasket = {
+  apples: 5,
+  oranges: 10,
+  grapes: 1000
+}
+
+//1
+for (let i = 0; i < basket.length; i++) {
+  console.log(basket[i]);
+}
+
+//2
+basket.forEach(item => {
+  console.log(item);
+})
+
+for (item in detailedBasket) {
+  console.log(item);
+}
+
+for (item of basket) {
+  console.log(item);
+}
+
+// Question #1:
+// create a function called biggestNumberInArray() that takes
+// an array as a parameter and returns the biggest number.
+// biggestNumberInArray([-1,0,3,100, 99, 2, 99]) should return 100;
+// Use at least 3 different types of javascript loops to write this:
+const array = [-1,0,3,100, 99, 2, 99] // should return 100
+const array2 = ['a', 3, 4, 2] // should return 4
+const array3 = [] // should return 0
+
+function biggestNumberInArray(arr) {
+  let highest = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (highest < array[i]) {
+      highest = array[i];
+    }
+  }
+  return highest
+}
+
+function biggestNumberInArray2(arr) {
+  let highest = 0;
+  array.forEach(item => {
+    if (highest < item) {
+      highest = item;
+    }
+  })
+  return highest;
+}
+
+function biggestNumberInArray3(arr) {
+  let highest = 0;
+  for (item of array) {
+    if (highest < item) {
+      highest = item;
+    }
+  }
+  return highest;
+}
+
+
+biggestNumberInArray3(array3)
+
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+function checkBasket(basket, lookingFor) {
+  for (item in basket) {
+    if (item === lookingFor) {
+      return `${lookingFor} is in your basket`
+    }
+  }
+  return 'that does not exist in your basket'
+}
